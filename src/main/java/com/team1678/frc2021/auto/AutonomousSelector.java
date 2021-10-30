cce3b2cde9ceac40486cc32695a61991eece3b78
emxia18 <emxia18@gmail.com>
2021-10-30 14:02:09 -0700

    add right five autos to auto selector

--- src/main/java/com/team1678/frc2021/auto/AutonomousSelector.java
+++ src/main/java/com/team1678/frc2021/auto/AutonomousSelector.java
@@ -24,10 +24,15 @@ public class AutonomousSelector {
 
         autonomousModeChooser = new SendableChooser<>();
         autonomousModeChooser.addOption("Right Ten Far Ball", AutonomousMode.RIGHT_TEN_FAR_BALL);
+        autonomousModeChooser.addOption("Right Five Near Ball", AutonomousMode.RIGHT_FIVE_NEAR_BALL);
+        autonomousModeChooser.addOption("Right Five Far Ball", AutonomousMode.RIGHT_FIVE_FAR_BALL);
+
         autonomousModeChooser.addOption("Left Eight Near Ball", AutonomousMode.LEFT_EIGHT_NEAR_BALL);
         autonomousModeChooser.addOption("Left Eight Far Ball", AutonomousMode.LEFT_EIGHT_FAR_BALL);
+
         autonomousModeChooser.addOption("Left Six Far Ball", AutonomousMode.LEFT_SIX_FAR_BALL);
         autonomousModeChooser.addOption("Left Six Near Ball", AutonomousMode.LEFT_SIX_NEAR_BALL);
+        
         autonomousModeChooser.addOption("Shot Left Back", AutonomousMode.SHOT_LEFT_BACK);
         autonomousModeChooser.addOption("Shot Left Front", AutonomousMode.SHOT_LEFT_FRONT);
         autonomousModeChooser.addOption("Shot Center Back", AutonomousMode.SHOT_CENTER_BACK);
@@ -45,20 +50,28 @@ public class AutonomousSelector {
         switch (mode) {
             case TEST_STRAIGHT_PATH:
                 return new TestStraightPath(s_Swerve);
+
             case RIGHT_TEN_BALL:
                 return new RightTenMode(s_Swerve);
             case RIGHT_TEN_NEAR_BALL:
                 return new RightTenNearMode(s_Swerve);
             case RIGHT_TEN_FAR_BALL:
                 return new RightTenFarMode(s_Swerve);
+            case RIGHT_FIVE_NEAR_BALL:
+                return new RightFiveNearMode(s_Swerve);
+            case RIGHT_FIVE_FAR_BALL:
+                return new RightFiveFarMode(s_Swerve);
+            
             case LEFT_EIGHT_NEAR_BALL:
                 return new LeftEightNearMode(s_Swerve);
             case LEFT_EIGHT_FAR_BALL:
                 return new LeftEightFarMode(s_Swerve);
+
             case LEFT_SIX_FAR_BALL:
                 return new LeftSixFarMode(s_Swerve);
             case LEFT_SIX_NEAR_BALL:
                 return new LeftSixNearMode(s_Swerve);
+
             case SHOT_LEFT_BACK:
                 return new ShotLeftBack(s_Swerve);
             case SHOT_LEFT_FRONT:
@@ -67,6 +80,7 @@ public class AutonomousSelector {
                 return new ShotCenterBack(s_Swerve);
             case SHOT_CENTER_FRONT:
                 return new ShotCenterFront(s_Swerve);
+
             case AIM_TEST_AUTO:
                 return new AimTestAuto(s_Swerve);
             default:
@@ -82,17 +96,25 @@ public class AutonomousSelector {
 
     private enum AutonomousMode {
         TEST_STRAIGHT_PATH,
+
         RIGHT_TEN_BALL,
         RIGHT_TEN_NEAR_BALL,
         RIGHT_TEN_FAR_BALL,
+
+        RIGHT_FIVE_NEAR_BALL,
+        RIGHT_FIVE_FAR_BALL,
+
         LEFT_EIGHT_NEAR_BALL,
         LEFT_EIGHT_FAR_BALL,
+
         LEFT_SIX_FAR_BALL,
         LEFT_SIX_NEAR_BALL,
+
         SHOT_LEFT_BACK,
         SHOT_LEFT_FRONT,
         SHOT_CENTER_BACK,
         SHOT_CENTER_FRONT,
+
         AIM_TEST_AUTO
     }
 
