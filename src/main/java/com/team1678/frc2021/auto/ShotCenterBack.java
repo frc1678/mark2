791c67dd2af55c73a08f37c88a62ed4cef67e5d5
emxia18 <emxia18@gmail.com>
2021-10-30 09:10:23 -0700

    change starting orientation of center back

--- src/main/java/com/team1678/frc2021/auto/ShotCenterBack.java
+++ src/main/java/com/team1678/frc2021/auto/ShotCenterBack.java
@@ -56,7 +56,7 @@ public class ShotCenterBack extends SequentialCommandGroup{
             new AutoAimCommand(mSuperstructure, 180);
 
         addCommands(
-            new InstantCommand(() -> s_Swerve.resetOdometry(new Pose2d(2.90, 0.71, Rotation2d.fromDegrees(270.0)))),
+            new InstantCommand(() -> s_Swerve.resetOdometry(new Pose2d(2.90, 5.84, Rotation2d.fromDegrees(270.0)))),
             aim,
             shoot,
             moveBackCommand
