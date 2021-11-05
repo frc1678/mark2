abae397ce30f1640cf0f6a0de2035f1be899a897
apurvagyan <apurvagyan@gmail.com>
2021-11-05 16:54:46 -0700

    deadband logic to test increased sensitivity

--- src/main/java/com/team1678/frc2021/commands/TeleopSwerve.java
+++ src/main/java/com/team1678/frc2021/commands/TeleopSwerve.java
@@ -3,6 +3,7 @@ package com.team1678.frc2021.commands;
 import com.team1678.frc2021.Constants;
 import com.team1678.frc2021.subsystems.Swerve;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
@@ -34,26 +35,42 @@ public class TeleopSwerve extends CommandBase {
         this.openLoop = openLoop;
     }
 
-    private double applyDeadband(double input){
+    private double applyRotationalDeadband(double input){
         double deadband = Constants.stickDeadband;
-        if(Math.abs(input) < deadband){
+        if (Math.abs(input) < deadband) {
             return 0.0;
         } else {
-            return (input - (Math.signum(input) * deadband))/(1 - deadband);
+            return (input - (Math.signum(input) * deadband)) / (1 - deadband);
         }
+    }
+
+    private Translation2d applyTranslationalDeadband(Translation2d input) {
+        double deadband = Constants.stickDeadband;
+        if (input.getNorm() < deadband) {
+            return new Translation2d();
+        } else {
+            Rotation2d deadband_direction = new Rotation2d(input.getX(), input.getY());
+            Translation2d deadband_vector = new Translation2d(Constants.stickDeadband, deadband_direction);
+
+            double scaled_x = input.getX() - (Math.signum(input.getX()) * deadband_vector.getX()) / (1 - deadband_vector.getX());
+            double scaled_y = input.getY() - (Math.signum(input.getY()) * deadband_vector.getY()) / (1 - deadband_vector.getY());
+            return new Translation2d(scaled_x, scaled_y);
+        }
+
     }
 
     public double[] getAxes() {
         double yAxis = -controller.getRawAxis(translationAxis);
         double xAxis = -controller.getRawAxis(strafeAxis);
         double rAxis = -controller.getRawAxis(rotationAxis);
+
+        Translation2d tAxes;
         
         /* Deadbands */
-        yAxis = applyDeadband(yAxis);
-        xAxis = applyDeadband(xAxis);
-        rAxis = applyDeadband(rAxis);
-        
-        double[] axes = {yAxis, xAxis, rAxis};
+        tAxes = applyTranslationalDeadband(new Translation2d(yAxis, xAxis));
+        rAxis = applyRotationalDeadband(rAxis);
+
+        double[] axes = {tAxes.getX(), tAxes.getY(), rAxis};
 
         return axes;
     }
@@ -65,17 +82,18 @@ public class TeleopSwerve extends CommandBase {
         double xAxis;
         double rAxis;
 
+        Translation2d tAxes; // translational axis
+
         /* Inversions */
         yAxis = Constants.Swerve.invertYAxis ? controller.getRawAxis(translationAxis) : -controller.getRawAxis(translationAxis);
         xAxis = Constants.Swerve.invertXAxis ? controller.getRawAxis(strafeAxis) : -controller.getRawAxis(strafeAxis);
         rAxis = Constants.Swerve.invertRAxis ? controller.getRawAxis(rotationAxis) : -controller.getRawAxis(rotationAxis);
 
         /* Deadbands */
-        yAxis = applyDeadband(yAxis);
-        xAxis = applyDeadband(xAxis);
-        rAxis = applyDeadband(rAxis);
+        tAxes = applyTranslationalDeadband(new Translation2d(yAxis, xAxis));
+        rAxis = applyRotationalDeadband(rAxis);
 
-        translation = new Translation2d(yAxis, xAxis).times(Constants.Swerve.maxSpeed);
+        translation = new Translation2d(tAxes.getX(), tAxes.getY()).times(Constants.Swerve.maxSpeed);
         rotation = rAxis * Constants.Swerve.maxAngularVelocity;
         s_Swerve.drive(translation, rotation, fieldRelative, openLoop);
     }
