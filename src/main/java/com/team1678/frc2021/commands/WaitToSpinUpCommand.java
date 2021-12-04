559d5f0592cfd7c880d8900cefd36f9a4aa073ac
Yi Zhang <yi.zhang.yzfl@gmail.com>
2021-12-04 13:24:09 -0800

    remove unused import

--- src/main/java/com/team1678/frc2021/commands/WaitToSpinUpCommand.java
+++ src/main/java/com/team1678/frc2021/commands/WaitToSpinUpCommand.java
@@ -1,6 +1,5 @@
 package com.team1678.frc2021.commands;
 
-import com.team1678.frc2021.subsystems.Shooter;
 import com.team1678.frc2021.subsystems.Superstructure;
 
 import edu.wpi.first.wpilibj.Timer;
